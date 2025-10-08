c84f479fb9096d8b3cc9fcf4224f0b7034ca0eea
خالد المنتصر <k774468815k@gmail.com>
2025-10-09 00:37:05 +0300

    Create dashboardService.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ src/dashboard/dashboardService.js
@@ -0,0 +1,11 @@
+export function getDashboardData() {
+  // محاكاة بيانات لوحة التحكم
+  return {
+    user: "خالد المنتصر",
+    stats: {
+      projects: 5,
+      messages: 12,
+      notifications: 3
+    }
+  };
+}
