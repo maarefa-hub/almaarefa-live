e36877dbf9098885c9561a7bd4264ea951ba72dd
خالد المنتصر <k774468815k@gmail.com>
2025-10-09 00:44:56 +0300

    Create uiService.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ src/ui/uiService.js
@@ -0,0 +1,9 @@
+import { createButton } from "./button.js";
+import { createInput } from "./input.js";
+import { createCard } from "./card.js";
+
+export const UIService = {
+  createButton,
+  createInput,
+  createCard
+};
