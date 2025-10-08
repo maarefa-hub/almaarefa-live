f15e6e11475cba96780c6f1c54fd200a69dd4a01
خالد المنتصر <k774468815k@gmail.com>
2025-10-09 01:31:14 +0300

    Create user.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ src/user/user.js
@@ -0,0 +1,12 @@
+import { getUser, updateUser } from "./userService.js";
+import { renderUserProfile } from "./userView.js";
+
+export function initUser() {
+  const user = getUser();
+  renderUserProfile(user);
+}
+
+export function handleUserUpdate(data) {
+  updateUser(data);
+  renderUserProfile(getUser());
+}
